fb5b904c91e53858fb47257029e7b555ef056bb3
Akhtar, Shoaib (HBO) <Shoaib.Akhtar@hbo.com>
2016-10-26 23:50:42 -0400

    Upgrded to Camden.SR1 and Boot 1.4.1. Fixd all deprecation issues found.

--- microservices/support/discovery-server/src/test/java/se/callista/microservises/support/discovery/ApplicationTests.java
+++ microservices/support/discovery-server/src/test/java/se/callista/microservises/support/discovery/ApplicationTests.java
@@ -4,22 +4,18 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = EurekaApplication.class)
-@WebAppConfiguration
-@IntegrationTest("server.port=0")
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = EurekaApplication.class, webEnvironment= SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "server.port=0")
 @Ignore
 public class ApplicationTests {
 	
