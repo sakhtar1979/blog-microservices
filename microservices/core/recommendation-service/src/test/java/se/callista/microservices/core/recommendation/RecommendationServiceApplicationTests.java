baa340d171895c94385d979f8d952c12983f5922
Akhtar, Shoaib (HBO) <Shoaib.Akhtar@hbo.com>
2016-10-26 23:51:54 -0400

    Upgraded to Camden.SR1 and Boot 1.4.1. Fixed all deprecation issues found.

--- microservices/core/recommendation-service/src/test/java/se/callista/microservices/core/recommendation/RecommendationServiceApplicationTests.java
+++ microservices/core/recommendation-service/src/test/java/se/callista/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -2,16 +2,13 @@ package se.callista.microservices.core.recommendation;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.IntegrationTest;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = RecommendationServiceApplication.class)
-@WebAppConfiguration
+
+@RunWith(SpringRunner.class)
 // Instruct embedded Tomcat to run on a random free port and skip talking to the Config, Bus and Discovery server
-@IntegrationTest({"server.port=0", "spring.cloud.config.enabled=false", "spring.cloud.bus.enabled=false", "spring.cloud.discovery.enabled=false"})
+@SpringBootTest(classes = RecommendationServiceApplication.class, webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT, properties = {"server.port=0", "spring.cloud.config.enabled=false", "spring.cloud.bus.enabled=false", "spring.cloud.discovery.enabled=false"})
 public class RecommendationServiceApplicationTests {
 
 	@Test
